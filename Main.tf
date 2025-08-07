025db004aeda694335a84ef2d3b53b4c22c84575
Gaurav <gaurav.shivchauhan@gmail.com>
2025-08-07 19:06:47 +0530

    modified one rg

--- Main.tf
+++ Main.tf
@@ -12,8 +12,8 @@ provider "azurerm" {
   subscription_id = "b2744a0d-e7f7-4cea-af52-689c2e4a2c69"
 }
 
-resource "azurerm_resource_group" "rg2" {
-  name     = "Rg-2"
+resource "azurerm_resource_group" "rg1" {
+  name     = "Rg-1"
   location = "West Europe"
 }
 
